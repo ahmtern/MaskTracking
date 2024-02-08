2d46d982f2ac00be4fa6a93adaa26bc0beb323a8
Ahmet EREN <155841151+ahmtern@users.noreply.github.com>
2024-02-08 14:49:03 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Concrete;
+
+namespace Workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "AHMET";
+            person1.LastName = "EREN";
+            person1.DateOfBirthYear = 1997;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Osman";
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+    }
+}
